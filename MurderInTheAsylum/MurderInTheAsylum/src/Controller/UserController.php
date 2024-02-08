22934eea8ca23ea71e1a9dd75e08fc2e089d1c95
IsabelArellano <115017603+IsabelArellano@users.noreply.github.com>
2024-02-08 16:12:37 +0100

    Endpoints de PUT(editar el perfil del usuario)/POST(loggear al usuario)/GET(listar los usuarios)/DELETE(eliminar usuario)

--- MurderInTheAsylum/MurderInTheAsylum/src/Controller/UserController.php
+++ MurderInTheAsylum/MurderInTheAsylum/src/Controller/UserController.php
@@ -26,29 +26,29 @@ class UserController extends AbstractController
     public function registerUser(Request $request): Response
     {
         $requestData = json_decode($request->getContent(), true);
-    
+
         $nickname = $requestData['nickname_user'] ?? null;
         $email = $requestData['email_user'] ?? null;
-    
+
         // Check if nickname or email is not provided
         if (!$nickname || !$email) {
             return $this->json(['error' => 'El nickname y el correo electrónico son obligatorios'], Response::HTTP_BAD_REQUEST);
         }
-    
+
         // Check if a user with the provided nickname or email already exists
         $userRepository = $this->entityManager->getRepository(Users::class);
         $existingUser = $userRepository->findOneBy(['nicknameUser' => $nickname]);
-    
+
         if ($existingUser) {
             return $this->json(['error' => 'El nickname ya está registrado, elige otro'], Response::HTTP_BAD_REQUEST);
         }
-    
+
         $existingEmailUser = $userRepository->findOneBy(['emailUser' => $email]);
-    
+
         if ($existingEmailUser) {
             return $this->json(['error' => 'El correo electrónico ya está registrado, elige otro'], Response::HTTP_BAD_REQUEST);
         }
-    
+
         // If no existing user, proceed with registration
         $user = new Users();
         $user->setNameUser($requestData['name_user'] ?? null);
@@ -58,18 +58,20 @@ class UserController extends AbstractController
         $user->setEmailUser($email);
         $user->setPhoneUser($requestData['phone_user'] ?? null);
         $user->setPassword($requestData['password'] ?? null);
-    
+
         $this->entityManager->persist($user);
         $this->entityManager->flush();
-    
+
         return $this->json(['message' => 'Usuario creado con éxito'], Response::HTTP_CREATED);
     }
 
     #[Route('/server/user/login', name: 'user_login', methods: ['POST'])]
     public function loginUser(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $nickname = $request->query->get('nickname_user');
-        $password = $request->query->get('password');
+        $requestData = json_decode($request->getContent(), true);
+
+        $nickname = $requestData['nickname_user'] ?? null;
+        $password = $requestData['password'] ?? null;
 
         // Verificar que se proporcionó el apodo y la contraseña
         if (!$nickname || !$password) {
@@ -88,12 +90,84 @@ class UserController extends AbstractController
             return $this->json(['error' => 'Credenciales inválidas'], Response::HTTP_UNAUTHORIZED);
         }
 
+        // Si llegamos aquí, el inicio de sesión fue exitoso
+        return $this->json(['message' => 'Inicio de sesión exitoso'], Response::HTTP_OK);
+    }
+
+    #[Route('/server/user/edit/{nickname}', name: 'user_edit', methods: ['PUT'])]
+    public function updateUserByNickname(Request $request, $nickname): Response
+    {
+        $userRepository = $this->entityManager->getRepository(Users::class);
+        $user = $userRepository->findOneBy(['nicknameUser' => $nickname]);
+    
+        if (!$user) {
+            throw $this->createNotFoundException('No se encontró el usuario con el nickname: ' . $nickname);
+        }
+    
+        $requestData = json_decode($request->getContent(), true);
+    
+        // Verificar y actualizar los campos proporcionados en la solicitud
+        if (isset($requestData['name_user'])) {
+            $user->setNameUser($requestData['name_user']);
+        }
+        if (isset($requestData['surname_user'])) {
+            $user->setSurnameUser($requestData['surname_user']);
+        }
+        if (isset($requestData['age_user'])) {
+            $user->setAgeUser($requestData['age_user']);
+        }
+        if (isset($requestData['email_user'])) {
+            $user->setEmailUser($requestData['email_user']);
+        }
+        if (isset($requestData['phone_user'])) {
+            $user->setPhoneUser($requestData['phone_user']);
+        }
+        if (isset($requestData['password'])) {
+            $user->setPassword($requestData['password']);
+        }
+        if (isset($requestData['image_user'])) {
+            $user->setImageUser($requestData['image_user']);
+        }
+    
+        $this->entityManager->flush();
+    
+        return $this->json(['message' => 'Usuario actualizado con éxito']);
+    }
+    
+
+    #[Route('/server/user/delete/{nickname}', name: 'user_delete', methods: ['DELETE'])]
+    public function deleteUserByNickname($nickname): Response
+    {
+        $userRepository = $this->entityManager->getRepository(Users::class);
+        $user = $userRepository->findOneBy(['nicknameUser' => $nickname]);
+    
+        if (!$user) {
+            throw $this->createNotFoundException('No se encontró el usuario con el nickname: ' . $nickname);
+        }
+    
+        $this->entityManager->remove($user);
+        $this->entityManager->flush();
+    
+        return $this->json(['message' => 'Usuario eliminado con éxito']);
+    }
+    
+
+    #[Route('/server/user/{nickname}', name: 'user_by_nickname', methods: ['GET'])]
+    public function getUserByNickname($nickname): Response
+    {
+        $userRepository = $this->entityManager->getRepository(Users::class);
+        $user = $userRepository->findOneBy(['nicknameUser' => $nickname]);
+    
+        if (!$user) {
+            return $this->json(['error' => 'Usuario no encontrado'], Response::HTTP_NOT_FOUND);
+        }
+    
         $imageData = null;
         if ($user->getImageUser() !== null) {
             // Convertir blob a base64
             $imageData = base64_encode(stream_get_contents($user->getImageUser()));
         }
-
+    
         $userData = [
             'id_user' => $user->getIdUser(),
             'name_user' => $user->getNameUser(),
@@ -105,53 +179,10 @@ class UserController extends AbstractController
             'password' => $user->getPassword(),
             'image_user' => $imageData,
         ];
-
+    
         return $this->json($userData, Response::HTTP_OK);
     }
-
-
- 
-    #[Route('/server/user/edit/{id}', name: 'user_edit', methods: ['PUT'])]
-    public function updateUser(Request $request, int $id): Response
-    {
-        $userRepository = $this->entityManager->getRepository(Users::class);
-        $user = $userRepository->find($id);
-
-        if (!$user) {
-            throw $this->createNotFoundException('No se encontró el usuario con el ID: ' . $id);
-        }
-
-        $requestData = json_decode($request->getContent(), true);
-
-        $user->setNameUser($requestData['name_user'] ?? $user->getNameUser());
-        $user->setSurnameUser($requestData['surname_user'] ?? $user->getSurnameUser());
-        $user->setNicknameUser($requestData['nickname_user'] ?? $user->getNicknameUser());
-        $user->setAgeUser($requestData['age_user'] ?? $user->getAgeUser());
-        $user->setEmailUser($requestData['email_user'] ?? $user->getEmailUser());
-        $user->setPhoneUser($requestData['phone_user'] ?? $user->getPhoneUser());
-        $user->setPassword($requestData['password'] ?? $user->getPassword());
-        $user->setImageUser($requestData['image_user'] ?? $user->getImageUser());
-
-        $this->entityManager->flush();
-
-        return $this->json(['message' => 'Usuario actualizado con éxito']);
-    }
-
-    #[Route('/server/user/delete/{id}', name: 'user_delete', methods: ['DELETE'])]
-    public function deleteUser(int $id): Response
-    {
-        $userRepository = $this->entityManager->getRepository(Users::class);
-        $user = $userRepository->find($id);
-
-        if (!$user) {
-            throw $this->createNotFoundException('No se encontró el usuario con el ID: ' . $id);
-        }
-
-        $this->entityManager->remove($user);
-        $this->entityManager->flush();
-
-        return $this->json(['message' => 'Usuario eliminado con éxito']);
-    }
+    
 }
 
 
